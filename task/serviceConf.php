0934786a52dac9687ffca8c3868b4a66dc408437
张晓亮 <xiaoliang9@staff.weibo.com>
2019-03-15 18:09:36 +0800

    配置文件

---
+++ task/serviceConf.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * catprocess config
+ * @by xiaoliang
+ */
+return $config = [
+    'serviceMark'      => 'memberConfig', //服务标识
+
+    // 进程相关
+    'processName' => ':member', // 设置进程名, 方便管理, 默认值 swooleTopicQueue
+
+    //
+
+    // 资源相关
+    'redis'            => [
+        'host'  => '127.0.0.1',
+        'port'  => '6379',
+        'preKey'=> 'task1-',
+        //'password'=>'',
+    ],
+
+    // exec任务相关,name的名字不能相同
+    'exec'      => [
+        [
+            'name' => 'member_1',// 名称即使文件名，请慎用
+            'max_request' => 0, // 限制进程最大请求数 0=不限制请求书  >0超出销毁
+            'memory_limit' => 50, // 单位:MB 最大内存限制，超出将自动销毁重新启动
+            'workNum'   => 2
+        ],
+    ],
+];
